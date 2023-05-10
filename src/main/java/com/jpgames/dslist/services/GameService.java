a28f48838e967df0c3cb26c879733de466e71f29
Pamplona <jppamplona101@gmail.com>
2023-05-10 20:52:51 -0300

    get name by id

--- src/main/java/com/jpgames/dslist/services/GameService.java
+++ src/main/java/com/jpgames/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jpgames.dslist.dto.GameDTO;
 import com.jpgames.dslist.dto.GameMinDTO;
 import com.jpgames.dslist.entidades.Game;
 import com.jpgames.dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game r = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(r);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> r = gameRepository.findAll();
 		return r.stream().map(x -> new GameMinDTO(x)).toList();
